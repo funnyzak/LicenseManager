89ae65b97e85b86bb334404d2c19af2edb5e795a
Leon <silenceace@gmail.com>
2024-11-17 18:44:45 +0800

    docs: 更新 README 文件，优化项目结构说明和测试运行指令；重构硬件信息采集测试

--- tests/test_license_validator.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from license_manager.validator.license_validator import LicenseValidator
-
-class TestLicenseValidator(unittest.TestCase):
-    def test_validate_license(self):
-        validator = LicenseValidator()
-        is_valid = validator.validate_license('license.lic', 'hardware.id')
-        self.assertTrue(is_valid)
